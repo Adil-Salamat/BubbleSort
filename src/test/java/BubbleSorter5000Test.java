c934ce2a0a03d8e29a0e91d325272e0dd725d6fa
Adil-Salamat <zzz-boring@hotmail.co.uk>
2022-05-11 15:31:41 +0100

    Bubble sort v1, works with negatives,
    Added tests

--- src/test/java/BubbleSorter5000Test.java
+++ src/test/java/BubbleSorter5000Test.java
@@ -1,5 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSorter5000Test {
+  @Test
+  public void bubbleSortTest(){
+    int[] unsorted = {5,4,3,2,1};
+    int[] sorted = {1,2,3,4,5};
 
+    int[] actuallySorted = BubbleSorter5000.bubbleSorter5000( unsorted );
+
+    assertThat(actuallySorted).isEqualTo(sorted);
+  }
+
+  @Test
+  public void bubbleSortTestNeg(){
+    int[] unsorted = {-5,4,3,-2,1};
+    int[] sorted = {-5,-2,1,3,4};
+
+    int[] actuallySorted = BubbleSorter5000.bubbleSorter5000( unsorted );
+
+    assertThat(actuallySorted).isEqualTo(sorted);
+  }
+  @Test
+  public void bubbleSortTestPresorted(){
+    int[] unsorted = {1,2,3,4,5};
+    int[] sorted = {1,2,3,4,5};
+
+    int[] actuallySorted = BubbleSorter5000.bubbleSorter5000( unsorted );
+
+    assertThat(actuallySorted).isEqualTo(sorted);
+  }
 }
